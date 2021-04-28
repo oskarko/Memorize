d0a08534d950ff3c58addefd5c677c549503e654
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-04-28 11:59:02 +0200

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Oscar Rodriguez Garrucho on 28/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
